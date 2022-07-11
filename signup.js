c14e37e603f396ad04b2503436443d04c1b621c7
Trust Akalonu <70828775+Trust-coder1@users.noreply.github.com>
2022-07-11 04:03:03 +0100

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,9 @@
+// const show =  document.getElementById("show")
+// const password = document.getElementById("password")
+// // Show password when user click on the button
+// show.addEventListener("click", () => {
+//     password.type="text";
+// })
+// show.addEventListener("click", () => {
+//     password.type="password";
+// })
